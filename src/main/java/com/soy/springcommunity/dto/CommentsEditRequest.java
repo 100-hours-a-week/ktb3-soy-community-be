0df6999de54d1f26658ab51eeffc2ac9d8a1d87b
soykeepgoing <soykeepgoing@gmail.com>
2025-11-27 16:37:03 +0900

    refactor: Post, Comment 도메인에서 생성, 수정 요청에 @Valid 어노테이션 추가

--- src/main/java/com/soy/springcommunity/dto/CommentsEditRequest.java
+++ src/main/java/com/soy/springcommunity/dto/CommentsEditRequest.java
@@ -1,9 +1,11 @@
 package com.soy.springcommunity.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class CommentsEditRequest {
+    @NotBlank
     private String newCommentContent;
     public CommentsEditRequest() {}
     public CommentsEditRequest(String newCommentContent) {
